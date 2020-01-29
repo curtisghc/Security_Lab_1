e6a12497649741efa61fc06e6b6bbf7315583050
Curtis Gallagher <curtisghc@gmail.com>
2020-01-29 12:16:51 -0500

    added functions to read and write keys

--- encrypter.py
+++ encrypter.py
@@ -1,18 +1,43 @@
 from des import DesKey
+import random
+import string
 
-key0 = DesKey(b"test key")
+def keygen(length=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
 
-data = "one two three four"
+def write_key(key):
+    with open("key.txt", 'w') as fp:
+        fp.write(key)
+    print("Key written to \"key.txt\"")
 
-print("original")
-print(data)
+def read_key():
+    with open("key.txt", 'r') as fp:
+        return fp.read()
 
-encrypted_data = key0.encrypt(data.encode('utf-8'), padding=True)
+should_generate = input("Generate new key? (y/n)")
 
-print("encrypted:")
-print(encrypted_data)
+if(should_generate == "y"):
+    key = keygen(8)
+    write_key(key)
+else:
+    key = read_key()
 
-decrypted_data = key0.decrypt(encrypted_data, padding=True)
+key0 = DesKey(key.encode('utf-8'))
 
-print("decrypted:")
-print(decrypted_data.decode('utf-8'))
+#ip_str = input("Enter the ip to chat")
+
+print("Write messages, \'quit\' to quit")
+
+while (True):
+    data = input(">")
+    if(data == 'quit'):
+        break
+    encrypted_data = key0.encrypt(data.encode('utf-8'), padding=True)
+    print(encrypted_data)
+
+
+    decrypted_data = key0.decrypt(encrypted_data, padding=True)
+
+    print('decrypted')
+    print(decrypted_data.decode('utf-8'))
