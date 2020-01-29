6578477e27f06c35b580ebdfbb79157a6aaef222
Curtis Gallagher <curtisghc@gmail.com>
2020-01-29 13:15:15 -0500

    added chat and multithreading using client and server, added to readme

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# Security_Lab_1
-security lab 1
+# Security_Lab_1 security lab 1 ## Install des library using pip3 on ubuntu
 
-## Install des library using pip3 on ubuntu
+## Dependencies
+Currently Python2.7 and 3 are needed, will fix later.
 ```
 sudo apt install python3-pip
 sudo pip3 install des
 ```
+
+## Usage
+This package provides a server hosted chatroom, where messages are encrypted at endpoints using DES.
+
+Run the server in python2 for now.
+To run as a demo, use the loopback address and a random port number:
+```
+python server.py 127.0.0.1 9909
+```
+
+Then clients connect using the same ip and port number:
+```
+python3 client.py 127.0.0.1 9909
+```
+
+Each connection and message will be displayed on the server, but unreadable without the secret key.
+
+Key must be placed in the running directory as "key.txt".
+
+## TODO
+Currently decrypting messages gives a bug, has to do with the padding of messages sent over the sockets. Will fix.
---
+++ client.py
@@ -0,0 +1,79 @@
+# Python program to implement client side of chat room.
+import socket
+import select
+import sys
+from des import DesKey
+import random
+import string
+
+def keygen(length=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+def write_key(key):
+    with open("key.txt", 'w') as fp:
+        fp.write(key)
+    print("Key written to \"key.txt\"")
+
+def read_key():
+    with open("key.txt", 'r') as fp:
+        return fp.read()
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+    print("Correct usage: script, IP address, port number")
+    exit()
+
+should_generate = input("Generate new key? (y/n)")
+
+if(should_generate == "y"):
+    key = keygen(8)
+    write_key(key)
+else:
+    key = read_key()
+
+key0 = DesKey(key.encode('utf-8'))
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+    print("Correct usage: script, IP address, port number")
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+server.connect((IP_address, Port))
+
+while True:
+
+    # maintains a list of possible input streams
+    sockets_list = [sys.stdin, server]
+
+    """ There are two possible input situations. Either the
+    user wants to give  manual input to send to other people,
+    or the server is sending a message  to be printed on the
+    screen. Select returns from sockets_list, the stream that
+    is reader for input. So for example, if the server wants
+    to send a message, then the if condition will hold true
+    below.If the user wants to send a message, the else
+    condition will evaluate as true"""
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            """
+            print(message)
+            if(message[0] == '_'):
+                print(message[1:])
+            else:
+            """
+            message = key0.decrypt(message, padding=True)
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            message = key0.encrypt(message.encode('utf-8'), padding=True)
+            server.send(message)
+            sys.stdout.flush()
+server.close()
